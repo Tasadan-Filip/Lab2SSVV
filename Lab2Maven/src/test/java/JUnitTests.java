afef0a547c830fed77420e955f3dc3cbe8f7bc2b
Tasadan Filip <tasadan.filip@gmail.com>
2023-03-17 19:24:03 +0200

    Initial commit

---
+++ Lab2Maven/src/test/java/JUnitTests.java
@@ -0,0 +1,36 @@
+import domain.Student;
+import org.junit.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class JUnitTests {
+    @Test
+    public void lab2TestMethod() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        //StudentFileRepository studentFileRepository = new StudentFileRepository(filenameStudent);
+        //TemaFileRepository temaFileRepository = new TemaFileRepository(filenameTema);
+        //NotaValidator notaValidator = new NotaValidator(studentFileRepository, temaFileRepository);
+        //NotaFileRepository notaFileRepository = new NotaFileRepository(filenameNota);
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        Student student = new Student("1", "Test", 937, "test@gmail.com");
+        Student student1 = service.addStudent(student);
+        assertTrue(service.findStudent("1") != null);
+    }
+}
